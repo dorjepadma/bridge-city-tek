12e3ac943bfc17d8a299611518883f0eea828291
dorjepadma <dorjepadma@gmail.com>
2021-08-22 23:35:50 -0700

    added bridge and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Bridge from './assets/images/portlandBridge.jpeg'
 
 function App() {
   return (
     <div className="App">
+      <img src={Bridge} className="App-image" alt="A view of the Portland Bridge" />
+      <div className="App-header">
       Welcome to Bridge City Tech! 
-      Web and Software development for Portland. 
-      
+      Web and Software based in Portland, Oregon. 
+      </div>  
     </div>
   );
 }
