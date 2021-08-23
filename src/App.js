4938d93349e613736771bcdb0da2a0e230aeae83
dorjepadma <dorjepadma@gmail.com>
2021-08-23 13:19:36 -0700

    housecleaning for production build

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import Bridge from './assets/images/portlandBridge.jpeg'
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
+
+      <h1>
+      Bridge City Tech
+      </h1>
+      </div>
       <img className= 'bridge' src={Bridge} className="App-image" alt="A view of the Portland Bridge" />
       <div className="App-header">
-      Welcome to Bridge City Tech! 
-      Web and Software based in Portland, Oregon. 
+      
+      Web development based in Portland, Oregon. 
       </div>  
+      <div className="App-header">
+      contact: dorje@bridgecitytek.com 
+      </div> 
     </div>
   );
 }
