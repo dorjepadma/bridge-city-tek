a1a992360bd7ffe5b4338bab6aea908e27ccfb02
dorjepadma <dorjepadma@gmail.com>
2022-01-28 11:36:07 -0800

    theme colors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 
 import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
 import Header from './components/Header/Header.component.jsx';
+import theme from './components/ui/Theme.js';
+import Button from '@mui/material/Button';
 
 import Bridge from './assets/images/portlandBridge.jpeg'
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}> 
       <Header />
       <div className="App-header">
-
       <h1>
       Bridge City Tech
       </h1>
       </div>
-      <img className= 'bridge' src={Bridge} className="App-image" alt="A view of the Portland Bridge" />
+      <img className= 'bridge' src={Bridge} alt="A view of the Portland Bridge" />
       <div className="App-header">
-      
       Web development based in Portland, Oregon. 
       </div>  
       <div className="App-header">
       contact: dorje@bridgecitytek.com 
       </div> 
-    </div>
+      <Button color="secondary" variant="contained"> Color Test</Button>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, useScrollTrigger } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+// import { theme } from "./ui/Theme";
 
 
-export default function Header() {
+function ElevationScroll(props) {
+  const { children } = props;
+  //elevates header when scrolling
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 20,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header(props) {
   return (
-    <AppBar>
+    <ElevationScroll>
+      <ThemeProvider > 
+    <AppBar position='fixed' color="primary">
       <Toolbar>Bridge City Tech</Toolbar>
     </AppBar>
+    </ThemeProvider>
+    </ElevationScroll>
   );
 }
