f724923c5d69f73671c7e73d7ad703e780122797
dorjepadma <dorjepadma@gmail.com>
2022-08-08 12:57:09 -0700

    contact page formed, about page formed

---
+++ src/pages/Contact/contact.component.jsx
@@ -0,0 +1,18 @@
+import './contact.styles.scss';
+
+const Contact = () => {
+  return (
+    <div className='contactContainer'>
+      <div variant='h1' className="contactTitle">
+        Contact
+      </div>
+      <div className='contact'>
+        <div variant="h4" className="contactCard">
+        If you have any questions or concerns, please feel free to contact me: dorje@bridgecitytek.com
+        </div>  
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
